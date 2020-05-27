a872452bd5ad918d7731eff94674dd4dfaf06ff3
gbroiles <gbroiles@gmail.com>
2020-05-26 18:50:59 -0700

    reformat with black module to move closer to PEP8 standards

--- script.py
+++ script.py
@@ -6,9 +6,10 @@ import sys
 from email.mime.text import MIMEText
 from datetime import timedelta, date
 
-import keyring ## Install it `pip install keyring`, if you are using KWallet please see: https://github.com/jaraco/keyring#installation---linux
+import keyring  ## Install it `pip install keyring`, if you are using KWallet please see: https://github.com/jaraco/keyring#installation---linux
+
 ## Install it `pip install notify-run`, it's what make me send you the notifications
-# from notify_run import Notify 
+# from notify_run import Notify
 
 
 def check_day():
@@ -25,17 +26,21 @@ def check_day():
         tomorrow_date = []
         tomorrow_date_hash = []
 
-   ## Check for the date as i said above!
+    ## Check for the date as i said above!
     if today_date == str(date.today()):
-        input("\n\nYou already checked today, Please come tomorrow!\n\n(Press Enter To Leave!)\n\n")
+        input(
+            "\n\nYou already checked today, Please come tomorrow!\n\n(Press Enter To Leave!)\n\n"
+        )
         exit()
-    
-    elif tomorrow_date == str(date.today()) and tomorrow_date_hash == str(encrypt_date(date.today())):
+
+    elif tomorrow_date == str(date.today()) and tomorrow_date_hash == str(
+        encrypt_date(date.today())
+    ):
         login()
-    
+
     elif today_date == [] and tomorrow_date == [] and tomorrow_date_hash == []:
         login()
-    
+
     else:
         send_email()
         sys.exit(0)
@@ -44,19 +49,23 @@ def check_day():
 def login():
     """This Function check for password and password saved in your keyring so do not worry!"""
 
-    user_password_input = getpass.getpass("Please type your password(or `exit` to leave or press ctrl + c): ") ## Here is your password. If it's right you will be able to login to add tomorrow's date
+    user_password_input = getpass.getpass(
+        "Please type your password(or `exit` to leave or press ctrl + c): "
+    )  ## Here is your password. If it's right you will be able to login to add tomorrow's date
 
     if user_password_input == user_password():
         add_day()
-    
+
     elif user_password_input.lower() == "exit":
         sys.exit(0)
-    
+
     elif user_password_input != user_password():
         ## I know code messy but this code give you more 3 chances to add right password
         def retry_password():
             for password_again in range(3):
-                user_password_again = getpass.getpass("Password is wrong, please try again (it's case sensitive): ")
+                user_password_again = getpass.getpass(
+                    "Password is wrong, please try again (it's case sensitive): "
+                )
                 if user_password_again == user_password():
                     add_day()
                     return True
@@ -71,27 +80,44 @@ def login():
 def user_password():
     """This function to add your password in keyring incase if its not"""
 
-    app_name = 'Dead Man Swtich APP'
-    user_name = getpass.getuser() ##Gets your username, you not need it actually it just to know for which user password is set 
+    app_name = "Dead Man Swtich APP"
+    user_name = (
+        getpass.getuser()
+    )  ##Gets your username, you not need it actually it just to know for which user password is set
 
     ## If password not found in your keyring it will ask you to add one
     if keyring.get_password(app_name, user_name) == None:
-        user_password_input = input("I assume it's your first time here so please add your password(first time only and it's case sensitive): ")
+        user_password_input = input(
+            "I assume it's your first time here so please add your password(first time only and it's case sensitive): "
+        )
         keyring.set_password(app_name, user_name, user_password_input)
-
 
     user_password = keyring.get_password(app_name, user_name)
     return user_password
 
 
-
-
 def add_day():
     """This function add the tomorrow's date into the data file"""
 
-    write_to_files("data.txt", "w", "Today Date: " + "\n" + str(date.today()) + "\n\n" + "Tomorrow Date: " + "\n" + str(date.today() + timedelta(days=1)) + "\n\n" + "Tomorrow Hash: " + "\n" + str(encrypt_date(date.today() + timedelta(days=1))))
+    write_to_files(
+        "data.txt",
+        "w",
+        "Today Date: "
+        + "\n"
+        + str(date.today())
+        + "\n\n"
+        + "Tomorrow Date: "
+        + "\n"
+        + str(date.today() + timedelta(days=1))
+        + "\n\n"
+        + "Tomorrow Hash: "
+        + "\n"
+        + str(encrypt_date(date.today() + timedelta(days=1))),
+    )
 
-    input("\n\nGlad to hear that you are alive!\ncome back tomorrow!\n\n(Press Enter To Leave!)\n\n")
+    input(
+        "\n\nGlad to hear that you are alive!\ncome back tomorrow!\n\n(Press Enter To Leave!)\n\n"
+    )
 
     ## Use this code if you want to "decrypt" the base64, for me i not need it --until now--
     # decrypt = base64.decodebytes(today_date)
@@ -102,9 +128,9 @@ def encrypt_date(data):
     """This function converts the tomorrow's date into base64 as kind of hash to ensure no one played in the dates"""
 
     day = str(data)
-    day_date = day.encode('utf-8')
+    day_date = day.encode("utf-8")
     encrypt = base64.b64encode(day_date)
-    return encrypt.decode('utf-8')
+    return encrypt.decode("utf-8")
 
 
 def read_from_files(fileName, accessMode):
@@ -117,7 +143,7 @@ def read_from_files(fileName, accessMode):
     except FileNotFoundError:
         input("Please create a file called 'data.txt'\n\n(Press Enter To Leave!)\n\n")
     except:
-        input("\n\nOops, something went wrong\n\n(Press Enter To Leave!)\n\n")   
+        input("\n\nOops, something went wrong\n\n(Press Enter To Leave!)\n\n")
 
 
 def write_to_files(fileName, accessMode, data):
@@ -127,10 +153,11 @@ def write_to_files(fileName, accessMode, data):
         file = open(fileName, accessMode)
         file.write(data)
     except:
-        input("\n\nOops, something went wrong while writing date to files\n\n(Press Enter To Leave!)\n\n")    
+        input(
+            "\n\nOops, something went wrong while writing date to files\n\n(Press Enter To Leave!)\n\n"
+        )
     finally:
         file.close()
-
 
 
 def send_email():
@@ -138,12 +165,17 @@ def send_email():
 
     print("\n\nSeems you dead, RIP i'm going to send emails now\n\n")
 
-    s = smtplib.SMTP('smtp.gmail.com', 587) ##You can change SMTP to your email's one, just ask their support! and type your login data as it's same as you just login with their login page
+    s = smtplib.SMTP(
+        "smtp.gmail.com", 587
+    )  ##You can change SMTP to your email's one, just ask their support! and type your login data as it's same as you just login with their login page
     s.starttls()
-    s.login("YOUR_EMAIL@gmail.com", "YOUR_PASSWORD") ##Type your email and password here to login
+    s.login(
+        "YOUR_EMAIL@gmail.com", "YOUR_PASSWORD"
+    )  ##Type your email and password here to login
     s.set_debuglevel(1)
-    msg = MIMEText("""BODY OF THE EMAIL (The message)""", "plain") ##Type your message here and Change "plain" to "html" (with quotes) if you want to type HTML email instead!
-
+    msg = MIMEText(
+        """BODY OF THE EMAIL (The message)""", "plain"
+    )  ##Type your message here and Change "plain" to "html" (with quotes) if you want to type HTML email instead!
 
     ## An example of HTML email and you could use https://html5-editor.net/ as help if you do not know HTML
 
@@ -159,18 +191,18 @@ def send_email():
     #     </html>
     #     """, 'html')
 
-
-    sender = 'YOUR_EMAIL@gmail.com' ##Type your email again or your name/username
-    recipients = ['YOUR_Friend@gmail.com', 'Your_Family@hotmail.com', 'Your_Teacher@yahoo.com'] ##type people to send them the email here
-    msg['Subject'] = "SUBJECT OF THE EMAIL (The Title/Name)" ##subject of the email
-    msg['From'] = sender
-    msg['To'] = ", ".join(recipients)
+    sender = "YOUR_EMAIL@gmail.com"  ##Type your email again or your name/username
+    recipients = [
+        "YOUR_Friend@gmail.com",
+        "Your_Family@hotmail.com",
+        "Your_Teacher@yahoo.com",
+    ]  ##type people to send them the email here
+    msg["Subject"] = "SUBJECT OF THE EMAIL (The Title/Name)"  ##subject of the email
+    msg["From"] = sender
+    msg["To"] = ", ".join(recipients)
     s.sendmail(sender, recipients, msg.as_string())
-    
+
     input("\n\nEmail(s), Sent!\n\n(Press Enter To Leave!)\n\n")
-
-
-
 
 
 ## Enable it when you are on phone, due its useless on PC because script run on startup only but on phone it will be run most of time
@@ -198,5 +230,3 @@ def send_email():
 
 ## Start the app
 check_day()
-
-
